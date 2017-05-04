b66bf86b3d441372785b37d8287c01c64e119716
WinGoodyGoody <stevencameronaustralia@gmail.com>
2017-05-04 16:17:41 +1000

    Upload Proj2

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Forward!");
+            GetComponent<Rigidbody>().AddForce(transform.forward * 15);
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("Back!");
+            GetComponent<Rigidbody>().AddForce(transform.forward * -10);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Right!");
+            GetComponent<Rigidbody>().AddRelativeTorque(Vector3.up * 15);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left!");
+            GetComponent<Rigidbody>().AddRelativeTorque(Vector3.up * -15);
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/cannon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cannon : MonoBehaviour
+{
+    public GameObject bullet;
+    public float[] numbers;
+    float countTotal=0;
+
+	//Count for cannon balls or bullets
+	public int numberBullets;
+	public Text countText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //print( Add(x, y));
+        print(Add(numbers));
+		// Initialise number of bullets to zero
+		numberBullets = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+			numberBullets = numberBullets + 1;
+			countText.text = "Cannon Balls Fired: " + numberBullets.ToString ();
+        }
+    }
+
+    void Fire()
+	// Bring bullet into existance
+    { 
+		Instantiate(bullet, transform.position, transform.rotation); 
+	}
+
+    float Add (float[] numbers)
+    {
+       
+
+        for (int i = 0; i <  numbers.Length; i++)
+        {
+            countTotal += numbers[i];
+                }
+        return countTotal;
+    }
+}
